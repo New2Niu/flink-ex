f31a0f698fe35cd98e51acf95e87c9195249e6cf
New2Niu <raymailfox@foxmail.com>
2017-05-16 14:41:24 +0800

    D:/programs/Git/StateTest.java

--- src/main/scala/com/ray/test/StateTest.java
+++ src/main/scala/com/ray/test/StateTest.java
@@ -10,12 +10,15 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+
 /**
  * Created by Administrator on 2017-5-11.
  */
 public class StateTest {
 
     public static void main(String[] args) {
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.fromElements(Tuple2.of(2L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 4L), Tuple2.of(1L, 2L))
                 .keyBy(0)
